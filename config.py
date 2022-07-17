f62334e0ab3faa1c224a89dd1061000b931bd1fa
Ninzalo <108398873+Ninzalo@users.noreply.github.com>
2022-07-17 20:53:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 # token_vk = ""
 # group_id = 123
-token_vk = "ac24a219bdd1b0f51cccd0c5d299076713684dfa1971b2b7928c769655efe99fc5d405794a39751d03b54"
-group_id = '210817116' 
+token_vk = ""
+group_id = '' 
 
 db_path = f'{os.getcwd()}\\Lib\\db\\schedule_vk.db'
 data_folder = f'{os.getcwd()}\\data'
